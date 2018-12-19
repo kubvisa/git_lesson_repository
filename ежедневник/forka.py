91c182a6b50972ad1f3dfd40f56cc6574d2b974f
Ученик <kseniyazinurovna@gmail.com>
2018-12-19 19:15:22 +0600

    dorabotka

--- ежедневник/forka.py
+++ ежедневник/forka.py
@@ -1,17 +1,9 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'untitled1.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.3
-#
-# WARNING! All changes made in this file will be lost!
 import sys
 from untitled import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -23,8 +15,8 @@ class Ui_MainWindow(object):
         self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
         self.textEdit.setGeometry(QtCore.QRect(80, 10, 391, 161))
         self.textEdit.setObjectName("textEdit")
-        self.commandLinkButton = QtWidgets.QCommandLinkButton(self.centralwidget)
-        self.commandLinkButton.setGeometry(QtCore.QRect(340, 490, 185, 41))
+        self.commandLinkButton = perehod1(self.centralwidget)
+        self.commandLinkButton.setGeometry(QtCore.QRect(440, 490, 91, 41))
         self.commandLinkButton.setObjectName("commandLinkButton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -41,16 +33,23 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.textEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:48pt; color:#7f2c70;\">Ежедневник 2019</span></p></body></html>"))
-        self.commandLinkButton.setText(_translate("MainWindow", "CommandLinkButton"))
-class Dyrdom(QMainWindow,Ui_MainWindow):
+        self.textEdit.setHtml(_translate("MainWindow",
+                                         "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                         "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                         "p, li { white-space: pre-wrap; }\n"
+                                         "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+                                         "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:48pt; color:#7f2c70;\">Ежедневник 2019</span></p></body></html>"))
+        self.commandLinkButton.setWhatsThis(_translate("MainWindow",
+                                                       "<html><head/><body><p><span style=\" font-size:12pt;\">Вперёд</span></p><p><br/></p></body></html>"))
+        self.commandLinkButton.setText(_translate("MainWindow", "Вперёд"))
+
+
+class Dyrdom(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Dyrdom()
